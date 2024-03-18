501ce03e057d6362ffa00b526b923bc32bfb141d
Dexite07 <126443539+Dexite07@users.noreply.github.com>
2024-03-18 03:59:18 +0000

    Yanzaltsan huvilbar

---
+++ idegener.js
@@ -0,0 +1,8 @@
+function hearted(){
+    var image = document.getElementById("omgg");
+    image.src='<https://cdn-icons-png.flaticon.com/128/2589/2589175.png>';
+}
+function next(){
+    const image = document.getElementById("omgg");
+    image.src='<https://cdn-icons-png.flaticon.com/512/57/57602.png>';
+}
