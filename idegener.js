c436e58f7704f13ba72d1e84448be4e85768f40c
Dexite07 <126443539+Dexite07@users.noreply.github.com>
2024-04-22 03:53:20 +0000

    ajlaasaa garah yum bol halna shuu

--- idegener.js
+++ idegener.js
@@ -3,6 +3,6 @@ function hearted(){
     image.src='<https://cdn-icons-png.flaticon.com/128/2589/2589175.png>';
 }
 function next(){
-    const image = document.getElementById("omgg");
+    const image=document.getElementById("omgg");
     image.src='<https://cdn-icons-png.flaticon.com/512/57/57602.png>';
 }
